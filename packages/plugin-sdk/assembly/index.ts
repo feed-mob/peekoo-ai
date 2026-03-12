36f7acd73ff6d50b560dd8bbc5ded14ff2f861b8
Richard Hao <richard@0xdev.dev>
2026-03-13 00:24:42 +0800

    fix(plugin-sdk): make AssemblyScript barrel exports compile

--- packages/plugin-sdk/assembly/index.ts
+++ packages/plugin-sdk/assembly/index.ts
@@ -9,11 +9,13 @@
  *   import { state, log, notify, schedule, config, badge, events } from "@peekoo/plugin-sdk";
  */
 
-export * as state from "./state";
-export * as log from "./log";
-export * as notify from "./notify";
-export * as schedule from "./schedule";
-export * as config from "./config";
-export * as badge from "./badge";
-export * as events from "./events";
+import * as state from "./state";
+import * as log from "./log";
+import * as notify from "./notify";
+import * as schedule from "./schedule";
+import * as config from "./config";
+import * as badge from "./badge";
+import * as events from "./events";
+
+export { state, log, notify, schedule, config, badge, events };
 export { ScheduleInfo, BadgeItem } from "./types";
